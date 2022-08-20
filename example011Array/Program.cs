3ba87adafc25c3c7a172ae696fd4113e322721ea
Vladimir <k.vovik15@yandex.ru>
2022-08-20 14:19:52 +0300

    add Example 011 Array

---
+++ example011Array/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+
+void PrintAray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintAray(array);
+
